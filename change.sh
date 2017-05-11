bff5fdbbbd4a30e6e41984ea376fe5ef22e8789e
Egemen Can Güler <egemencanguler@gmail.com>
2017-05-11 19:29:42 +0300

    results

--- change.sh
+++ change.sh
@@ -2,7 +2,9 @@ if [ $1 -gt 1 ]
 then
   cp batch1/*.jpg ./TurkerGaze/demo/imgs
   cp batch2/*.jpg ./demo/imgs/
+  echo "TurkerGaze Batch1 - WebGaze Batch2"
 else
   cp batch2/*.jpg ./TurkerGaze/demo/imgs
   cp batch1/*.jpg ./demo/imgs/
+  echo "TurkerGaze Batch2 - WebGaze Batch1"
 fi
